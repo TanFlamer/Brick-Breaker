2c12fafd1276d59484ac33f1008d3ce494b9deaa
TanFlamer <zxtan01@outlook.com>
2021-11-21 22:03:42 +0800

    Update DebugConsole.java
    Used adapter classes instead of implementing listeners to delete empty overrides.

--- DebugConsole.java
+++ DebugConsole.java
@@ -19,13 +19,13 @@ package test;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class DebugConsole extends JDialog implements WindowListener{
+public class DebugConsole extends JDialog {
 
     private static final String TITLE = "Debug Console";
-
 
     private JFrame owner;
     private DebugPanel debugPanel;
@@ -43,61 +43,36 @@ public class DebugConsole extends JDialog implements WindowListener{
         debugPanel = new DebugPanel(wall);
         this.add(debugPanel,BorderLayout.CENTER); //add debug panel to center
 
-
         this.pack(); //resize debug console
     }
 
     private void initialize(){ //call debug console
+
         this.setModal(true);
         this.setTitle(TITLE);
         this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         this.setLayout(new BorderLayout());
-        this.addWindowListener(this);
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                gameBoard.repaint();
+            }
+
+            @Override
+            public void windowActivated(WindowEvent windowEvent) { //when debug console loaded
+                setLocation(); //set debug console location
+                Ball b = wall.ball; //get ball
+                debugPanel.setValues(b.getSpeedX(),b.getSpeedY()); //show current ball speed on slider
+            }
+        });
+
         this.setFocusable(true);
     }
-
 
     private void setLocation(){ //set debug console location
         int x = ((owner.getWidth() - this.getWidth()) / 2) + owner.getX();
         int y = ((owner.getHeight() - this.getHeight()) / 2) + owner.getY();
         this.setLocation(x,y);
-    }
-
-
-    @Override
-    public void windowOpened(WindowEvent windowEvent) {
-
-    }
-
-    @Override
-    public void windowClosing(WindowEvent windowEvent) {
-        gameBoard.repaint();
-    }
-
-    @Override
-    public void windowClosed(WindowEvent windowEvent) {
-
-    }
-
-    @Override
-    public void windowIconified(WindowEvent windowEvent) {
-
-    }
-
-    @Override
-    public void windowDeiconified(WindowEvent windowEvent) {
-
-    }
-
-    @Override
-    public void windowActivated(WindowEvent windowEvent) { //when debug console loaded
-        setLocation(); //set debug console location
-        Ball b = wall.ball; //get ball
-        debugPanel.setValues(b.getSpeedX(),b.getSpeedY()); //show current ball speed on slider
-    }
-
-    @Override
-    public void windowDeactivated(WindowEvent windowEvent) {
-
     }
 }
